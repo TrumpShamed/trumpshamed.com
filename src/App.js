c14bf174d11c25ce50bbb67400d0f3e6445cf98e
TrumpShamed Admin <TrumpShamed@gmail.com>
2016-10-13 15:03:54 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>TrumpShamed</h2>
+        </div>
+        <p className="App-intro">
+          We'll add something here... eventually
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
